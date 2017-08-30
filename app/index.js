eae6e31fdc763b50bd4dd46ff48dfa8ff9717ea7
Alain Lemaire <jaxdid@gmail.com>
2017-08-30 11:00:51 +0100

    Adds Components dir, Popular component and basic styling

--- app/index.js
+++ app/index.js
@@ -1,16 +1,6 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+const App = require('../components/App')
 require('./index.css')
-
-class App extends React.Component {
-  render () {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
-
-}
 
 ReactDOM.render(<App />, document.getElementById('app'))
