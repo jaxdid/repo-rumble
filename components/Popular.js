46c64a25d9597956f2cc44cc759d3e69b9c04e55
Alain Lemaire <jaxdid@gmail.com>
2017-08-30 17:26:41 +0100

    Adds selectable languages nav bar and styling

--- components/Popular.js
+++ components/Popular.js
@@ -1,9 +1,45 @@
 const React = require('react')
 
 class Popular extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selectedLang: 'All'
+    }
+
+    this.selectLang = this.selectLang.bind(this)
+  }
+
+  selectLang (lang) {
+    this.setState({
+      selectedLang: lang
+    })
+  }
+
   render () {
+    const languages = [
+      'All',
+      'JavaScript',
+      'Ruby',
+      'Java',
+      'CSS',
+      'Python'
+    ]
+
     return (
-      <div>Popular</div>
+      <ul className='languages'>
+        {languages.map(lang => {
+          return (
+            <li
+              key={lang}
+              style={lang === this.state.selectedLang ? { color: '#d0021b' } : null}
+              onClick={this.selectLang.bind(null, lang)}
+            >
+              {lang}
+            </li>
+          )
+        })}
+      </ul>
     )
   }
 }
