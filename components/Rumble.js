67a6e54de03d593295a118d0dc10185040e41189
Alain Lemaire <jaxdid@gmail.com>
2017-09-01 00:22:57 +0100

    [WIP] Begins building out Rumble component

--- components/Rumble.js
+++ components/Rumble.js
@@ -1,13 +1,74 @@
 const React = require('react')
+const PropTypes = require('prop-types')
 
-class Rumble extends React.Component {
+class PlayerInput extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: ''
+    }
+  }
+
   render () {
     return (
+      <form action=""></form>
+    )
+  }
+}
+
+class Rumble extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneAvatar: null,
+      playerTwoAvatar: null
+    }
+
+    this.handleSubmit.bind(this)
+  }
+
+  handleSubmit (id, username) {
+    this.setState(() => {
+      const newState = {}
+      newState[`${id}Name`] = username
+      newState[`${id}Avatar`] = `https://github.com/${username}.png?size=200`
+      return newState
+    })
+  }
+
+  render () {
+    const playerOneName = this.state.playerOneName
+    const playerTwoName = this.state.playerTwoName
+    
+    return (
       <div>
-        Rumble!
+        <div className="row">
+          {!playerOneName &&
+            <PlayerInput
+              id="playerOne"
+              label="Player One"
+              onSubmit={this.handleSubmit}
+            />
+          }
+          {!playerTwoName &&
+            <PlayerInput
+              id="playerTwo"
+              label="Player Two"
+              onSubmit={this.handleSubmit}
+            />
+          }
+        </div>
       </div>
     )
   }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
 }
 
 module.exports = Rumble
