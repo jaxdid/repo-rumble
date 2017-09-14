b79f54290ab1a4e597944c12e3488e5daf6a5270
Alain Lemaire <jaxdid@gmail.com>
2017-09-15 00:36:37 +0100

    Abstracts PlayerPreview for use in Results view

--- components/Rumble.js
+++ components/Rumble.js
@@ -1,34 +1,7 @@
 const React = require('react')
 const PropTypes = require('prop-types')
 const { Link } = require('react-router-dom')
-
-function PlayerPreview (props) {
-  return (
-    <div>
-      <div className="column">
-        <img
-          className="avatar"
-          src={props.avatar}
-          alt={`Avatar for ${props.username}`}
-        />
-        <h2 className="username">@{props.username}</h2>
-      </div>
-      <button
-        className="reset"
-        onClick={props.onReset.bind(null, props.id)}
-      >
-        reset
-      </button>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-}
+const PlayerPreview = require('./PlayerPreview')
 
 class PlayerInput extends React.Component {
   constructor (props) {
